26a657a27abbceeed1ede1c5f30f15da67ed276d
test <test@example.com>
2026-03-18 17:41:49 +0900

    feat: deploy frontend to github pages

--- supabase.config.js
+++ supabase.config.js
@@ -1,4 +1,4 @@
 window.__SUPABASE_CONFIG__ = {
-  url: "https://YOUR_PROJECT_REF.supabase.co",
-  anonKey: "YOUR_SUPABASE_ANON_KEY",
+  url: "https://csarhidurfxdmcworbtk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzYXJoaWR1cmZ4ZG1jd29yYnRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MTIwNTgsImV4cCI6MjA4OTM4ODA1OH0.AW7mIgO0M_qk3xjrLkATrHO__HWFozcTyxjEIf-rjr8",
 };
